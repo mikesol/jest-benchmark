080d055c5e784d4ed4c47e14acf6d8eaab85fd8f
Mike Solomon <mike@meeshkan.com>
2019-06-04 09:57:10 +0300

    Initial commit

---
+++ src/p.test.js
@@ -0,0 +1,27 @@
+test('a + b', () => {
+	expect(1 + 2).toBe(3);
+});
+test('a + b', () => {
+	expect(1 + 2).toBe(3);
+});
+test('a + b', () => {
+	expect(1 + 2).toBe(3);
+});
+test('a + b', () => {
+	expect(1 + 2).toBe(3);
+});
+test('a + b', () => {
+	expect(1 + 2).toBe(3);
+});
+test('a + b', () => {
+	expect(1 + 2).toBe(3);
+});
+test('a + b', () => {
+	expect(1 + 2).toBe(3);
+});
+test('a + b', () => {
+	expect(1 + 2).toBe(3);
+});
+test('a + b', () => {
+	expect(1 + 2).toBe(3);
+});
